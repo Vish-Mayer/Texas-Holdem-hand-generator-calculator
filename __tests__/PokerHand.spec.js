72a8a1f9433fb670ed03b8ab77d1816bd3c91008
Vish Mayer <vish.mayer@gmail.com>
2021-11-27 10:58:21 +0000

    pokerhand.flush returns false if suits aren't the same

--- __tests__/PokerHand.spec.js
+++ __tests__/PokerHand.spec.js
@@ -7,9 +7,17 @@ describe("PokerHand", () => {
     expect(pokerhand.hand).toEqual(hand);
   });
 
-  it("returns true if the hand contains a flush", () => {
-    let hand = "AH KH QH JH TH";
-    let pokerhand = new PokerHand(hand);
-    expect(pokerhand.flush).toEqual(true);
+  describe("pokerhand.flush", () => {
+    it("returns true if the hand contains a flush", () => {
+      let hand = "AH KH QH JH TH";
+      let pokerhand = new PokerHand(hand);
+      expect(pokerhand.flush).toEqual(true);
+    });
+
+    it("returns false if the hand does not contain a flush", () => {
+      let hand = "AH KH QD JH TH";
+      let pokerhand = new PokerHand(hand);
+      expect(pokerhand.flush).toEqual(false);
+    });
   });
 });
--- src/PokerHand.js
+++ src/PokerHand.js
@@ -2,7 +2,7 @@ export class PokerHand {
   constructor(hand) {
     this.hand = hand;
     const cards = this.hand.split(" ");
-    const suits = cards.map(card => card[1]);
+    const suits = cards.map(card => card[1]).sort();
     this.flush = suits[0] === suits[4];
   }
 }
